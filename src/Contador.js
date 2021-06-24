fe85aad3d22e921bb935cf0110ffa09636e4c397
Nicolas Guzman Rivera <NNGR01@MacBook-Pro-de-Nicolas.local>
2021-06-24 15:41:11 -0400

    asd

---
+++ src/Contador.js
@@ -0,0 +1,37 @@
+import './Contador.css';
+
+const Contador = (props) => {
+    return (
+        <div className="container mt-5">
+                <div className="row">
+            <div className="col display-4">
+                    <i className="fa fa-clock"></i>
+                </div>
+                <div className="col display-4">
+                {props.num6}
+  </div>
+                <div className="col display-4">
+                {props.num5}
+  </div>
+                <div className="col display-4">
+                {props.num4}
+  </div>
+                <div className="col display-4">
+                {props.num3}
+  </div>
+                <div className="col display-4">
+                {props.num2}
+  </div>
+                <div className="col display-4">
+                    {props.num1}
+  </div>
+
+            </div>
+        </div>
+    )
+
+
+
+}
+
+export default Contador;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Contador from './Contador';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+let num4 = 0;
+let num5 = 0;
+let num6 = 0;
+
+
+
+
+setInterval (() => {
+
+    num1++;
+    
+    ReactDOM.render(<Contador 
+        num1={num1}
+        num2={num2}
+        num3={num3}
+        num4={num4}
+        num5={num5}
+        num6={num6}
+  
+  
+  />
+  , document.querySelector('#root'));
+
+
+},1000);
