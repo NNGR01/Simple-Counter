e467676e9b47361502d9a64f309da1a7a86c7165
Nicolas Guzman Rivera <NNGR01@MacBook-Pro-de-Nicolas.local>
2021-07-03 13:48:23 -0400

    contadorazo

--- src/index.js
+++ src/index.js
@@ -18,7 +18,33 @@ let num6 = 0;
 setInterval (() => {
 
     num1++;
+
+    if(num1 === 10){
+      num1 = 0
+      num2++
+    }
+    if(num2 === 6){
+      num2 = 0
+      num3++
+    }
+    if(num3 === 10){
+      num1 = 0
+      num4++
+    }  
     
+    if(num4 === 6){
+      num4 = 0
+      num5++
+    }
+    
+    if(num5 === 10){
+      num5 = 0 
+      num6++
+    }
+    if(num6 === 2 & num5 === 4){
+      num5 = 0
+      num6 = 0
+    } 
     ReactDOM.render(<Contador 
         num1={num1}
         num2={num2}
